dace334789b34d3ab9207ed99b3eb0a2db8039d3
Shashank <113260615+shashankaz@users.noreply.github.com>
2025-01-10 16:39:33 +0530

    fix: update import path for crypto.controller and add export for router

--- routes/crypto.routes.js
+++ routes/crypto.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { findDeviation, listStats } from "../controller/crypto.controller";
+import { findDeviation, listStats } from "../controller/crypto.controller.js";
 
 const router = Router();
 
 router.get("/stats", listStats);
 router.get("/deviation", findDeviation);
+
+export default router;
